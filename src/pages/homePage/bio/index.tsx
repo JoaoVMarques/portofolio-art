ad5b6c97a6c41d651b04ffc04799526c403461c3
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2024-07-27 16:05:09 -0300

    Adicionando imagem e melhorando o design da biografia

--- src/pages/homePage/bio/index.tsx
+++ src/pages/homePage/bio/index.tsx
@@ -1,11 +1,23 @@
+import { Col, Container, Row } from 'react-bootstrap';
+import placeholderImage from '../../../imgs/placeholder.png';
+
 function BioHomePage() {
   return (
-    <>
-      <h1>Bem-vindo ✨</h1>
-      <h3><a className="link-offset-2 link-underline link-underline-opacity-0 link-secondary" href="https://www.instagram.com/sleeppymiau/" target="_blank">🦋 Instagram</a></h3>
-      <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, ut?</h5>
-      <h5>Lorem ipsum dolor sit.</h5>
-    </>
+    <Container className='mt-5'>
+      <Row>
+        <Col>
+          <img src={placeholderImage} alt="placeholder imagem" />
+        </Col>
+        <Col className=''>
+          <h1 className='text-start'>Bem-vindo ✨</h1>
+          <h3 className='text-start'><a className="link-offset-2 link-underline link-underline-opacity-0 link-secondary" href="https://www.instagram.com/sleeppymiau/" target="_blank">🦋 Instagram</a></h3>
+          <h5 className='text-start mt-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, ut?
+          </h5>
+          <h5 className='text-start'>Lorem ipsum dolor sit.</h5>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
