a5801c4e6099e9e7e4ddd3e1a37e7d7d286fb1c6
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2024-08-05 20:45:00 -0300

    Adicionando pagina de galeria e deixando imagens de placeholder

--- src/pages/galeryPage/index.tsx
+++ src/pages/galeryPage/index.tsx
@@ -1,11 +1,33 @@
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 import Header from '../header';
-import placeholderImage from '../../imgs/placeholder.jpg';
+
+const images = [
+  'https://picsum.photos/200/300?image=1050',
+  'https://picsum.photos/300/300?image=201',
+  'https://picsum.photos/200/300?image=1001',
+  'https://picsum.photos/300/300?image=206',
+  'https://picsum.photos/300/300?image=206',
+  'https://picsum.photos/200/300?image=1050',
+];
 
 function GaleryPage() {
   return (
     <>
       <Header />
-      <img src={placeholderImage} alt="placeholder" />
+      <ResponsiveMasonry
+        columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 4 }}
+      >
+        <Masonry gutter='4px'>
+          {images.map((image, i) => (
+            <img
+              key={i}
+              src={image}
+              style={{ width: '100%', display: 'block' }}
+              alt=""
+            />
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
     </>
   );
 }
